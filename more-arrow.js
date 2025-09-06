e4bf7bd8e67b0195c729c764e57a95d678743a48
Kazi Fabiha Golam Liya <151756377+KaziFabihaLiya@users.noreply.github.com>
2025-09-06 22:35:46 +0600

    progress

---
+++ more-arrow.js
@@ -0,0 +1,19 @@
+const getTax = (amount,taxRate) => amount * taxRate/ 100;
+const add = (p,q) => p+q;
+
+//single parameter 
+const getSquare = (x) => x*2;
+const getHalf = (y) => y/2
+const firstElement = nums => nums[0];
+const logIt = () => console.log(78); // kono kisu return kore nah
+
+
+console.log(getSquare(10));
+console.log(getHalf(20));
+console.log(firstElement([45,20,5,30]));
+console.log(logIt());
+
+// anonymous function
+document.getElementById("").addEventListener('click', () =>{
+    
+});
